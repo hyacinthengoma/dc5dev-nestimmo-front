9295a43b93bea07af3c4bcbebdc1df8b891d04a3
Hyacinthe N'Goma <hyacinthengoma@gmail.com>
2024-07-16 11:06:46 +0200

    update front

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 import DrawerPost from "../post/DrawerPost";
+import {Button} from "@/components/ui/button";
 
 const Navbar = () => {
     return ( 
-        <nav className="flex justify-between p-5">
+        <nav className="flex justify-around bg-amber-400 p-5">
+            <Button variant="default">
             <Link href="/">Post list</Link>
+            </Button>
+
+            <Button variant="default">
             <Link href="/categories">Category list</Link>
+            </Button>
             <DrawerPost />
         </nav>
      );
